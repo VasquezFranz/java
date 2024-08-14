719d53a840bcea316af3eb686b24d5b787773adb
LAB-USR-LNORTE <LAB-USR-LNORTE@UTP313205.grupoutp.net>
2024-08-14 11:25:47 -0500

    primertcomit

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,11 @@
+package javaapplication1;
+
+import java.util.Scanner;
+
+int i;
+        
+        for (i=0; i < 10; i++) {
+            System.out.println("El valor de i es: "+i);
+        }
+    }
+}
